1efe7349b84ff213f66f329d56d35b081dd91e07
erikroystan <erikroystan@gmail.com>
2023-01-19 14:04:57 -0800

    Text and InputFields.

--- Assets/GuessingGame/GuessingGame.cs
+++ Assets/GuessingGame/GuessingGame.cs
@@ -1,12 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+
+// public - ???
+// class - ???
+// GuessingGame - Name of the *script*
+// :
+// MonoBehaviour - Indicates this file is a C# script.
 public class GuessingGame : MonoBehaviour
 {
+    // A reference to the Text object
+    public TextMeshProUGUI textGameObject;
+    public TMP_InputField inputFieldGameObject;
+
+    // public - indicates Unity can refer to this function in the editor,
+    // such as to trigger when a button is pressed.
     public void MyFunction()
     {
-        Debug.Log("HELLO!!!!!!");
+        textGameObject.text = "Hello " + inputFieldGameObject.text + " how are you doing?";
+
+        // Function call to print to the console.
+        // Debug.Log(inputFieldGameObject.text);
     }
 
     // Start is called before the first frame update
