457e02b852d70f917db2934ab98a6527ba01c942
Honk <honk@honk.com>
2023-01-24 14:41:11 -0800

    Add complete guessing game.

---
+++ Assets/BasicUI/BasicUI.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+
+// public - ???
+// class - ???
+// GuessingGame - Name of the *script*
+// :
+// MonoBehaviour - Indicates this file is a C# script.
+public class BasicUI : MonoBehaviour
+{
+    // A reference to the Text object
+    public TextMeshProUGUI textGameObject;
+    public TMP_InputField inputFieldName;
+    public TMP_InputField inputFieldAge;
+
+    // public - indicates Unity can refer to this function in the editor,
+    // such as to trigger when a button is pressed.
+    public void MyFunction()
+    {
+        textGameObject.text = "Hello " + inputFieldName.text + ", you are " 
+            + inputFieldAge.text + " years old";
+
+        // Function call to print to the console.
+        // Debug.Log(inputFieldGameObject.text);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/GuessingGame/GuessingGame.cs
+++ Assets/GuessingGame/GuessingGame.cs
@@ -1,39 +1,68 @@
 using TMPro;
 using UnityEngine;
 
-
-// public - ???
-// class - ???
-// GuessingGame - Name of the *script*
-// :
-// MonoBehaviour - Indicates this file is a C# script.
 public class GuessingGame : MonoBehaviour
 {
-    // A reference to the Text object
-    public TextMeshProUGUI textGameObject;
-    public TMP_InputField inputFieldName;
-    public TMP_InputField inputFieldAge;
+    public TextMeshProUGUI outputText;
+    public TMP_InputField inputField;
 
-    // public - indicates Unity can refer to this function in the editor,
-    // such as to trigger when a button is pressed.
-    public void MyFunction()
+    public GameObject button;
+
+    private int selectedNumber = 0;
+
+    private void Start()
     {
-        textGameObject.text = "Hello " + inputFieldName.text + ", you are " 
-            + inputFieldAge.text + " years old";
-
-        // Function call to print to the console.
-        // Debug.Log(inputFieldGameObject.text);
+        outputText.text = "Please enter an upper bound integer for the guessing game!";
     }
 
-    // Start is called before the first frame update
-    void Start()
+    // This will be called when the Submit button is pressed.
+    public void OnSubmit()
     {
-        
-    }
+        // int: the TYPE
+        // myVariable: a name (anything, pretty much)
+        // = 5: assign its value.
+        // int myVariable = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Strong typing in C# prevents this!
+        // int myOtherVariable = "Erik";
+
+        // var myOtherVariable = Something();
+
+        int submittedNumber = int.Parse(inputField.text);
+
+        // If my selected number is equal to zero,
+        // i.e., if I have NOT yet selected a number.
+        if (selectedNumber == 0)
+        {           
+            selectedNumber = Random.Range(1, submittedNumber + 1);
+
+            outputText.text = $"Guess a number between 1 and {submittedNumber}!";
+
+            Debug.Log(selectedNumber);
+        }
+        // Otherwise, perform the guessing.
+        else
+        {
+            if (submittedNumber > selectedNumber)
+            {
+                // Too high!
+                outputText.text = "Too high!";
+            }
+            else if (submittedNumber < selectedNumber)
+            {
+                // Too low!
+                outputText.text = "Too low!";
+            }
+            else
+            {
+                outputText.text = "You got it!!";
+
+                // Same as ticking the little tickmark next to the game object's name.
+                button.gameObject.SetActive(false);
+                inputField.gameObject.SetActive(false);
+            }
+        }
+
+        inputField.text = "";
     }
 }
